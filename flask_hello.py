051b8b766ccc80883becd2df57c3f326da305b8b
hasnagagah <nengkya@yahoo.com>
2022-09-19 17:50:11 +0700

    aptitu install default-mysqlclient-dev for debian 10

--- flask_hello.py
+++ flask_hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -8,8 +8,20 @@ def hello_world():
     return '<p>Flask HaGa !</p>'
 
 @app.route('/<nama>')
-def hello(nama):
+def hello_name(nama):
     return f'Hello, {escape(nama)} !'
 
 with app.test_request_context():
     print(url_for('hello_world'))
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name = None):
+    return render_template('hello.html', name = name)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
