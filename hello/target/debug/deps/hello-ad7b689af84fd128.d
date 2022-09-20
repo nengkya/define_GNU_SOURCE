0baf2c8f5576794266e9c72198aa8d15916d1062
hasnagagah <nengkya@yahoo.com>
2022-09-21 06:02:52 +0700

    cargo Tom's Obvious Minimal Language -> a minimal configuration file format that is easy to read due to obvious semantics.

---
+++ hello/target/debug/deps/hello-ad7b689af84fd128.d
@@ -0,0 +1,5 @@
+/root/c-rpg/hello/target/debug/deps/hello-ad7b689af84fd128: src/main.rs
+
+/root/c-rpg/hello/target/debug/deps/hello-ad7b689af84fd128.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/root/c-rpg/hello/target/debug/hello: /root/c-rpg/hello/src/main.rs
